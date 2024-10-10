70f052bb9345311ee0c51f02a5d593cdcaaaedbc
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2024-10-10 07:42:42 +0530

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -6,7 +6,7 @@ yum install httpd  -y
 systemctl  enable  httpd  --now
 firewall-cmd  --add-service={http,https} --permanent
 firewall-cmd  --reload
-mkdir /var/www/html/Rhce
-cp -rvf ./294/*  /var/www/html/Rhce
+cp -rvf 294  /var/www/html/
+mv /var/www/html/294   /var/www/html/Rhce
 curl  workstation.lab.example.com
 echo   practice makes a man perfect!!!!
