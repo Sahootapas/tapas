5ea0eb829c0a82e654a8a538f940f55912d25e78
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2024-10-10 07:34:18 +0530

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #when you run this script  make sure you have reseted your lab Thank you 
-mv ansible-posix-1.6.0 (1).tar.gz ansible-posix-1.6.0.tar.gz
 mv role1.tar.gz  balancer.tgz
 mv role2.tar.gz  phpinfo.tgz
 yum install httpd  -y
