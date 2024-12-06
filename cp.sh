d5c3d585207714907d19fc65dad582e46927529f
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2024-12-06 09:33:06 +0530

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #when you run this script  make sure you have reseted your lab Thank you 
-mv role1.tar.gz  balancer.tgz
-mv role2.tar.gz  phpinfo.tgz
-yum install httpd  -y
-systemctl  enable  httpd  --now
-firewall-cmd  --add-service={http,https} --permanent
-firewall-cmd  --reload
-mkdir /content
-cp -rvf ~/294  /content/
-echo   practice makes a man perfect!!!!
+#cyberphoton
+#try to be in tapas dir user 
+#after git clone do cd tapas 
+sudo yum install httpd -y 
+sudo  systemctl   enable   httpd  --now
+sudo firewall-cmd   --add-service=http --permanent
+sudo firewall-cmd --reload
+sudo mkdir  /var/www/html/content
+sudo systemctl restart httpd 
+sudo mv  ~/tapas/*      /var/www/html/content
+sudo chcon  system_u:object_r:httpd_sys_content_t:s0 /var/www/html/content/*
+echo   practice makes a man perfect!!!!   
+echo   Thanks  for this   oppertunity
